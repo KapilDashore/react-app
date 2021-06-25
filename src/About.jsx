b8e3585f7265a7f735639b7d32940e29ec169115
KapilDashore <kapildashore3389@gmail.com>
2021-06-25 23:21:41 +0530

    code pushed

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import web from "../src/images/team31.png"
+import Common from "./Common";
+
+const About = () => {
+    return (
+        <>
+            <Common
+                name="welcome to About page"
+                imgsrc={web}
+                visit="/contact"
+                btname="Contact Now"
+
+            />
+        </>
+    );
+};
+
+export default About;
---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+import web from "../src/images/webb.png";
+import app from "../src/images/app.jpg";
+import android from "../src/images/android.png";
+import digital from "../src/images/digital.png";
+import marketing from "../src/images/marketing.png";
+import software from "../src/images/software.png";
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "web development",
+    },
+    {
+        imgsrc: app,
+        title: "App development",
+    },
+    {
+        imgsrc: software,
+        title: "Software development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+    },
+    {
+        imgsrc: android,
+        title: "Android development",
+    },
+    {
+        imgsrc: marketing,
+        title: "Marketing",
+    }
+    
+]
+
+export default Sdata;
